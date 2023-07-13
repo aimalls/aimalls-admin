ba0b6e2aa83418d0f14ebaaa6b96cc620385ba32
hypergavs <hypergavs@gmail.com>
2023-07-13 16:04:59 +0800

    s

--- src/pages/public/auth/Login.tsx
+++ src/pages/public/auth/Login.tsx
@@ -68,7 +68,7 @@ const Login: React.FC = () => {
                                                 <div className='login-description'>Welcome back</div>
                                             </IonCol>
                                             <IonCol size='12' sizeSm='9' sizeMd='7'>
-                                                <IonButton expand='block' color={'light'} href={getGoogleAuthURL(import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URL)}>
+                                                <IonButton expand='block' color={'light'} size='large' href={getGoogleAuthURL(import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URL)}>
                                                     <img src={gmailIcon} alt="Google Icon" height={25} />
                                                     <div className='sign-up-title'>Sign In with Google</div>
                                                 </IonButton>
@@ -95,35 +95,22 @@ const Login: React.FC = () => {
                                                     required
                                                     onIonChange={(val) => setPassword(val.detail.value!)}
                                                 />
-                                                
-                                                <div className="forgot-password">
-                                                <IonButton href='/forgot-password' fill='clear'>
-                                                    <div className='forgot-password-title'>forgot password?</div>
-                                                </IonButton>
-                                                </div>
 
-                                                <IonButton expand='block' shape='round' size='large' onClick={() => processLogin()}>Login</IonButton>
+                                                <IonButton expand='block' shape='round' style={{marginTop: "50px"}} size='large' onClick={() => processLogin()}>Login</IonButton>
                                                 
                                             </IonCol>
                                         </IonRow>
                                     </div>
                                 </IonCol>
                                 <IonCol size='12' sizeSm='6' sizeMd='4' >
-                                    <div className="register-column">
+                                    <div className="welcome-admin-column">
                                         <IonRow className='ion-justify-content-center'>
                                             <IonCol size='12'>
                                                 <IonRouterLink routerLink='/'><div style={{ display: 'flex', justifyContent: 'center', marginBottom: "30px" }}><img src={ Logo } alt='aimalls' /></div></IonRouterLink>
-                                                <div className="register-title">New Here?</div>
-                                                <div className="register-description">
-                                                    Welcome to the future of shopping, where AI takes you on a personalized retail journey like never before. Sign up today and discover the extraordinary convenience, tailored experiences, and endless possibilities of AI malls.
+                                                <div className="welcome-admin-title">Welcome back to the AIMalls admin!</div>
+                                                <div className="welcome-admin-description">
+                                                We've missed your expertise in managing our virtual marketplace. Get ready to dive back into the world of e-commerce!
                                                 </div>
-                                            </IonCol>
-                                            <IonCol size='12' sizeMd='8'>
-                                                <IonButton size='large' expand='block' shape='round' routerLink='/register' color={'light'}>
-                                                    <div className="register-btn-txt">
-                                                        Register
-                                                    </div>
-                                                </IonButton>
                                             </IonCol>
                                         </IonRow>
                                     </div>
