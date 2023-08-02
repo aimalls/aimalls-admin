4b707b51f4c315a297b254792fdf9867a9c2974a
hypergavs <hypergavs@gmail.com>
2023-08-02 15:41:53 +0800

    s

--- src/pages/private/dashboard/roles/RolePolicies.tsx
+++ src/pages/private/dashboard/roles/RolePolicies.tsx
@@ -1,17 +1,42 @@
 import { FC } from "react";
-import { IonButton, IonCol, IonContent, IonGrid, IonPage, IonRow } from "@ionic/react";
+import { IonButton, IonCol, IonContent, IonGrid, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonLoading, IonPage, IonRow, useIonAlert, useIonLoading, useIonModal } from "@ionic/react";
 import { useQuery } from "@tanstack/react-query";
-import { getAllRolePoliciesFromAPI } from "../../../../requests/role-policy.request";
+import { getAllRolePoliciesFromAPI, iRolePolicy, processRolePolicyDeleteToAPI } from "../../../../requests/role-policy.request";
 export interface iProps {}
 export const RolePolicies: FC<iProps> = (props): JSX.Element => {
+
+    const [present, dismiss] = useIonLoading();
+    const [presentAlert] = useIonAlert();
 
     const rolePoliciesQuery = useQuery(["rolePoliciesQuery"], () => getAllRolePoliciesFromAPI())
 
 
-    const rolePolicies = rolePoliciesQuery.data;
+    const rolePolicies: iRolePolicy[] = rolePoliciesQuery.data;
+
+  
+
+
+
+    const processRolePolicyDelete = async (id: iRolePolicy['_id']) => {
+        try {
+            await present();                
+            const result = await processRolePolicyDeleteToAPI(id);
+
+            presentAlert(result.message)
+
+            rolePoliciesQuery.refetch()
+            
+        } catch (err: any) {
+            presentAlert(err)
+        } finally {
+            await dismiss();
+        }
+        
+    }
 
     return (
         <IonPage>
+            <IonLoading isOpen={ rolePoliciesQuery.isLoading } />
             <IonContent>
                 <IonGrid>
                     <IonRow>
@@ -21,7 +46,23 @@ export const RolePolicies: FC<iProps> = (props): JSX.Element => {
                         </IonCol>
                         { rolePolicies ? (
                             <IonCol size="12">
-                                { JSON.stringify(rolePolicies) }
+                                <IonList>
+                                    { rolePolicies.map(value => (
+                                        <IonItemSliding key={value._id}>
+                                            <IonItem>
+                                                <IonLabel>
+                                                    <div>{ value.name }</div>
+                                                    <p>{ value.createdAt }</p>
+                                                </IonLabel>
+                                                
+                                            </IonItem>
+                                            <IonItemOptions slot="end">
+                                                <IonItemOption color={"danger"} onClick={() => processRolePolicyDelete(value._id)}>Delete</IonItemOption>
+                                                <IonItemOption>Update</IonItemOption>
+                                            </IonItemOptions>
+                                        </IonItemSliding>
+                                    )) }
+                                </IonList>
                             </IonCol>
                         ): null }
                     </IonRow>
--- src/requests/role-policy.request.ts
+++ src/requests/role-policy.request.ts
@@ -4,7 +4,9 @@ export interface iRolePolicy {
     _id?: string,
     name: string,
     method_name: string,
-    description: string
+    description: string,
+    createdAt?: string,
+    updatedAt?: string
 }
 
 export const saveNewRolePolicyToAPI = (policy: iRolePolicy) => {
@@ -17,4 +19,10 @@ export const getAllRolePoliciesFromAPI = () => {
     return HTTP_API().get("/role-policy/get-all-role-policies")
         .then(response => response.data)
         .catch(err => Promise.reject(err))
+}
+
+export const processRolePolicyDeleteToAPI = (policy_id: iRolePolicy['_id']) => {
+    return HTTP_API().post("/role-policy/delete-role-policy", {policy_id})
+    .then(response => response.data)
+    .catch(err => Promise.reject(err))
 }
